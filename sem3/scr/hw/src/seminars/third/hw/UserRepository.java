38539ccf222d9e4d9ba809ff765631fd6ffd6739
Valera <leo.plv@bk.ru>
2023-10-30 15:54:02 +0500

    домашнее задание 3

---
+++ sem3/scr/hw/src/seminars/third/hw/UserRepository.java
@@ -0,0 +1,32 @@
+package sem3.scr.hw.src.seminars.third.hw;
+
+
+import java.util.*;
+
+public class UserRepository {
+
+    public List<User> data = new ArrayList<>();
+
+    public boolean addUser(User user) {
+        if (user.isAuth()) {
+            data.add(user);
+            return true;
+        }
+        return false;
+    }
+
+    public void logoutNonAdminUsers() {
+        for (User user : data) {
+            if (!user.isAdmin()) {
+                logoutUser(user);
+            } else {
+                user.setAuth(true);
+            }
+        }
+    }
+
+
+    public void logoutUser(User user) {
+        user.setAuth(false);
+    }
+}
